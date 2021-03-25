23148f5f0ba0b8f5b13b2ca27357be3d36d50a95
Helena Copppieters <hcoppieters@hotmail.com>
2021-03-25 16:46:28 +0100

    CategoriesController

--- PittigRestoMVC/Data/ApplicationDbContext.cs
+++ PittigRestoMVC/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PittigRestoMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,7 @@ namespace PittigRestoMVC.Data
             : base(options)
         {
         }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<SubCategory> SubCategory { get; set; }
     }
 }
